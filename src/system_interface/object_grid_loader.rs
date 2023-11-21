16df2f40e39ce7bd77d5934cedc4c1ff403b2d5b
Kyle Slugg <kyle.slugg@gmail.com>
2023-11-20 23:40:19 -0500

    Testing obj_grid file reader functionality. Currently troubleshooting bug related to TIFF sample bit -- look into how to set sample bits in QGIS (we want 32 or 64)

--- src/system_interface/object_grid_loader.rs
+++ src/system_interface/object_grid_loader.rs
@@ -1,26 +1,35 @@
-use image::{ImageError, Luma};
+use image::{io::Reader, ImageError, ImageFormat, Luma};
 use ndarray::Array2;
 
 pub fn read_raster(filepath: String) -> Result<Array2<u8>, ImageError> {
-    match image::open(filepath) {
-        Ok(img) => {
-            let (width, height) = (img.width(), img.height());
-            let mut binary_pixel_matrix = Array2::<u8>::default((width as usize, height as usize));
-            img.into_luma8()
-                .enumerate_pixels()
-                .for_each(|(col, row, p)| match p {
-                    Luma([0]) => {
-                        binary_pixel_matrix[[row as usize, col as usize]] = 0;
-                    }
-                    _ => {
-                        binary_pixel_matrix[[row as usize, col as usize]] = 0;
-                    }
-                });
+    match Reader::open(filepath)?.with_guessed_format() {
+        Ok(mut buffered_reader) => {
+            //buffered_reader.set_format(ImageFormat::Tiff);
+            match buffered_reader.decode() {
+                Ok(img) => {
+                    let (width, height) = (img.width(), img.height());
+                    let mut binary_pixel_matrix =
+                        Array2::<u8>::default((width as usize, height as usize));
+                    img.into_luma8()
+                        .enumerate_pixels()
+                        .for_each(|(col, row, p)| match p {
+                            Luma([0]) => {
+                                binary_pixel_matrix[[row as usize, col as usize]] = 0;
+                            }
+                            _ => {
+                                binary_pixel_matrix[[row as usize, col as usize]] = 0;
+                            }
+                        });
 
-            return Ok(binary_pixel_matrix);
+                    return Ok(binary_pixel_matrix);
+                }
+                Err(e) => {
+                    return Err(e);
+                }
+            }
         }
         Err(e) => {
-            return Err(e);
+            return Err(ImageError::IoError(e));
         }
     }
 }
